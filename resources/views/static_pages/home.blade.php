60888132d9210bdb3d267d05772bf8d3ddd01869
datangkang123 <2554509967@qq.com>
2019-07-12 19:46:46 +0800

    正确发送邮件

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,16 +2,14 @@
 
 @section('content')
   <div class="jumbotron">
-    <h1>Hello Laravel</h1>
+    <h2>系统简介</h2>
     <p class="lead">
-      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+      详细内容请查看 <a href="https://bk.dzbfsj.com/">具体操作方法</a> PHP通用查询系统。
     </p>
-    <p>
-      一切，将从这里开始。
-    </p>
-    <p>
-     <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
-    </p>
+    <p>使用须知：</p>
+    <p>1. 请勿将本系统用于政策法规不允许的用途；</p>
+    <p>2. 使用本系统请注意隐私保护；</p>
+    <p>3. 请用于本单位的查询，切勿冒充其他单位发布查询。 </p>
       <p>
       <a class="btn btn-lg btn-success" href="photo" role="button">上传excel</a>
     </p>
